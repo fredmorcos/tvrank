1139d67703a79872ae2f6d3cb9c7d4905e3ef6fd
Fred Morcos <fm@fredmorcos.com>
2021-12-07 10:34:13 +0100

    More steady progress bar and spinner

--- src/ui.rs
+++ src/ui.rs
@@ -4,7 +4,7 @@ use indicatif::{ProgressBar, ProgressStyle};
 
 pub fn create_progress_bar(msg: String, len: u64) -> ProgressBar {
   let bar = ProgressBar::new(len).with_style(ProgressStyle::default_bar().template(
-    "{msg}: {bar:40.cyan/blue} {percent}%  {bytes}/{total_bytes} {bytes_per_sec} {elapsed} ETA: {eta}",
+    "{msg}: {bar:40.cyan/blue} {percent:>3}%  {bytes:>9}/{total_bytes} {bytes_per_sec:>10} {elapsed:>4} ETA: {eta:>4}",
   ));
 
   bar.set_draw_rate(2);
@@ -15,11 +15,12 @@ pub fn create_progress_bar(msg: String, len: u64) -> ProgressBar {
 pub fn create_progress_spinner(msg: String) -> ProgressBar {
   let bar = ProgressBar::new_spinner().with_style(
     ProgressStyle::default_spinner()
-      .template("{msg}: {spinner:.cyan/blue}  {bytes} {bytes_per_sec} {elapsed}")
+      .template("{msg}: {spinner:.cyan/blue}  {bytes:>9} {bytes_per_sec:>10} {elapsed:>4}")
       .tick_strings(&[r"◧", r"◩", r"⬒", r"⬔", r"◨", r"◪", r"⬓", r"⬕"]),
   );
 
   bar.set_draw_rate(2);
+  bar.enable_steady_tick(100);
   bar.set_message(leak_string(msg));
   bar
 }
